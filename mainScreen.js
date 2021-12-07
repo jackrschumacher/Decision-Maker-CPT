0b0b61a18938c5212ce9cab9bd3e8ca21c800ad7
schujack-ctrl <84467112+schujack-ctrl@users.noreply.github.com>
2021-12-07 14:13:15 -0600

    Update mainScreen.js

--- mainScreen.js
+++ mainScreen.js
@@ -1,3 +1,15 @@
 var calories = "";
 var food_Type = "";
 var perpare_Time = "";
+
+
+function update_Vars(){
+  calories = getText("calories_Dropdown");
+  food_Type = getText("perpare_Time_Dropdown");
+  perpare_Time = getText("food_Type_Dropdown");
+}
+onEvent("choose_Button", "click", function( ) {
+  update_Vars();
+});
+
+
