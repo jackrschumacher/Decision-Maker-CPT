5a7324806624799d2fdc03399efd0cba36a98e09
schujack-ctrl <84467112+schujack-ctrl@users.noreply.github.com>
2021-12-08 17:22:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,19 +9,51 @@ var reccomend_Calories = "";
 
 
 function write_Text(){
-  setText("decision_Box","Your Choice:"+"We reccomend "+ reccomend+"."+ reccomend_Calories);
+  setText("decision_Box","Your Choice:"+"\n"+"We reccomend "+ reccomend+"."+ reccomend_Calories);
   
 }
 
 function update_Text(){
+  
   if (food_Type == "Grains"){
     if(perpare_Time == "<10" && calories == "Low"){
-      reccomend = "Oatmeal";
-      reccomend_Calories = "Try measuring the amount of oatmeal in order to eliminate excess.";
-      write_Text();
+        reccomend = "Oatmeal";
+        reccomend_Calories = "Try measuring the amount of oatmeal in order to eliminate excess.";
+        write_Text();
     }
-  }
+      else if(perpare_Time == "<10" && calories == "Medium"){
+        reccomend = "Oatmeal";
+        reccomend_Calories = "Try adding fruit to your outmeal.";
+        write_Text();
+  
+    }
+      else if(perpare_Time == "<10" && calories == "High"){
+        reccomend = "Oatmeal";
+        reccomend_Calories = "Try adding cocunut to your oatmeal.";
+        write_Text();
+    } 
+      else if(perpare_Time == ">10 and <20" && calories == "Low"){
+        reccomend = "Avacado Toast";
+        reccomend_Calories = "Try adding other vegtables such as tomatoes to your toast";
+        write_Text();
+        
+    }
+      else if(perpare_Time == ">10 and <20" && calories == "Low"){
+        reccomend = "Avacado Toast";
+        reccomend_Calories = "Try adding other Bagel Seasoning to your toast";
+        write_Text();
+        
+    }
+    else{
+      reccomend = "Invalid";
+    }
+
 }
+  
+}
+
+
+
 
 onEvent("choose_Button", "click", function( ) {
   calories = getText("calories_Dropdown");
