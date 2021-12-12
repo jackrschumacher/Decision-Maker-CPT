ca257c5b8bac4dd885d5bc55a370a4c4d93a438c
schujack-ctrl <84467112+schujack-ctrl@users.noreply.github.com>
2021-12-12 09:26:15 -0600

    Add Comments

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//Declare Variables
 var calories = "";
 var food_Type = "";
 var perpare_Time = "";
@@ -7,18 +9,23 @@ var reccomend_Calories = "";
 
 
 
-
+//Write Recommended Item to Screen
 function write_Text(){
   setText("decision_Box","Your Choice:"+"\n"+"\n"+"We reccomend "+ reccomend+"."+ reccomend_Calories+"\n"+"\n"+"Enjoy!");
   
 }
+//If Inputs are not valid, print Invalid, Play Error Input Sound
 function invalid(){
-  setText("decision_Box","Invalid"+"\n"+"\n"+"Please Try Again");
+  playSound("sound://category_retro/retro_game_echo_error_4.mp3", false);
+  setText("decision_Box", "INVALID!"+"\n"+"\n"+"Please Try Again");
+  
 }
 
+//Check each of the input boxes, and make a reccomendation based on this
 function update_Text(){
-  
+  //Grains Section
   if (food_Type == "Grains"){
+    //If Perpare time is less than 10 minutes, reccomend Oatmeal with various add-ons based on calorie amountm, send to write_Text()
     if(perpare_Time == "<10" && calories == "Low"){
         reccomend = "Oatmeal";
         reccomend_Calories = "Try measuring the amount of oatmeal in order to eliminate excess.";
@@ -34,7 +41,7 @@ function update_Text(){
         reccomend = "Oatmeal";
         reccomend_Calories = "Try adding cocunut to your oatmeal.";
         write_Text();
-    } 
+    }//If Perpare time is between 10 and 20 minutes, reccomend Avaccado Toast with various add-ons based on calorie amountm, send to write_Text()
       else if(perpare_Time == ">10 and <20" && calories == "Low"){
         reccomend = "Avacado Toast";
         reccomend_Calories = "Try adding other vegtables such as tomatoes to your toast";
@@ -50,7 +57,7 @@ function update_Text(){
         reccomend = "Avacado Toast";
         reccomend_Calories = "Try adding fruits and other vegtables to your meal.";
         write_Text();
-      }
+      }//If Perpare time is greater than 20 minutes, reccomend Pancakes with various add-ons based on calorie amountm, send to write_Text()
       else if(perpare_Time == ">20" && calories == "Low"){
         reccomend = "Pancakes";
         reccomend_Calories = "Try adding fruits to your pancake.";
@@ -67,10 +74,13 @@ function update_Text(){
         write_Text();
       }  
     }
+    //Else, Invalid, Send to invalid() to display error message
       else{
         invalid();
     }
+  //Meats Section
   if (food_Type == "Meats"){
+    //If Perpare time is Less than 10 minutes, reccomend Sausage with various add-ons based on calorie amountm, send to write_Text()
       if(perpare_Time == "<10" && calories == "Low"){
         reccomend = "Sausage";
         reccomend_Calories = "Try Cutting the Sausage in order to only eat as much as you need";
@@ -87,6 +97,7 @@ function update_Text(){
         reccomend_Calories = "Try eating toast with your sausage";
         write_Text();
     } 
+    //If Perpare time is between 10 and 20 minutes, reccomend a Breakfast Sandwhich with various add-ons based on calorie amountm, send to write_Text()
       else if(perpare_Time == ">10 and <20" && calories == "Low"){
         reccomend = "a Breakfast Sandwich";
         reccomend_Calories = "Try adding greens (spinach,etc) to your sandwich.";
@@ -103,6 +114,7 @@ function update_Text(){
         reccomend_Calories = "Try adding fruits to your meal.";
         write_Text();
       }
+      //If Perpare time is greater than 20 minutes, reccomend a Casserole with various add-ons based on calorie amountm, send to write_Text()
       else if(perpare_Time == ">20" && calories == "Low"){
         reccomend = "a Casserole";
         reccomend_Calories = "Try adding cheese to your casserole.";
@@ -118,11 +130,14 @@ function update_Text(){
         reccomend_Calories = "Try adding vegtables to your meal.";
         write_Text();
       } 
+      //Else, Invalid, Send to invalid() to display error message
       else{
         invalid();
       }
   }
+  //Protein Section
   if(food_Type == "Protein"){
+    //If Perpare time is Less than 10 minutes, reccomend Eggs with various add-ons based on calorie amountm, send to write_Text()
       if(perpare_Time == "<10" && calories == "Low"){
         reccomend = "Eggs";
         reccomend_Calories = "Try adding cheese to your egg.";
@@ -141,20 +156,21 @@ function update_Text(){
     }
       else if(perpare_Time == ">10 and <20" && calories == "Low"){
         reccomend = "Sausage and Potatoes";
-        reccomend_Calories = "Try adding other vegtables such as tomatoes to your Sausage and Potatoes";
+        reccomend_Calories = "Try adding other vegtables such as tomatoes to your Sausage and Potatoes.";
         write_Text();
         
-    }
+    }//If Perpare time is between 10 and 20 minutes, reccomend Sausage and Potatoes with various add-ons based on calorie amountm, send to write_Text()
       else if(perpare_Time == ">10 and <20" && calories == "Medium"){
         reccomend = "Sausage and Potatoes";
-        reccomend_Calories = "Try Cheese to your sausage and potatoes";
+        reccomend_Calories = "Try Cheese to your sausage and potatoes.";
         write_Text();
     } 
       else if(perpare_Time == ">10 and <20" && calories == "High"){
         reccomend = "Sausage and Potatoes";
-        reccomend_Calories = "Try putting toast bits in your sausage and potatoes";
+        reccomend_Calories = "Try putting toast bits in your sausage and potatoes.";
         write_Text();
     }
+    //If Perpare time is greater than 20 minutes, reccomend Eggs, Potatoes, and Suasage with various add-ons based on calorie amountm, send to write_Text()
       else if(perpare_Time == ">20" && calories == "Low"){
         reccomend = "Eggs, Patatoes, and Sausage";
         reccomend_Calories = "Try adding cheese to your meal.";
@@ -162,7 +178,7 @@ function update_Text(){
       }  
       else if(perpare_Time == ">20" && calories == "Medium"){
         reccomend = "Eggs, Patatoes, and Sausage";
-        reccomend_Calories = "Try having toast with your meal";
+        reccomend_Calories = "Try having toast with your meal.";
         write_Text();
       }  
       else if(perpare_Time == ">20" && calories == "High"){
@@ -170,6 +186,7 @@ function update_Text(){
         reccomend_Calories = "Try adding vegtables to your meal.";
         write_Text();
       } 
+      //Else, Invalid, Send to invalid() to display error message
       else{
         invalid();
       }
@@ -183,16 +200,19 @@ function update_Text(){
 
 
 
-
+//When Choose Button Clicked, Get text from all of the dropdowns, send this info to update_Text() Function (which will decide reccomendations), and play a sound and set screen to decision screen
 onEvent("choose_Button", "click", function( ) {
   calories = getText("calories_Dropdown");
   perpare_Time = getText("perpare_Time_Dropdown");
   food_Type = getText("food_Type_Dropdown");
+  update_Text();
   playSound("sound://category_app/app_interface_button_2.mp3", false);
   setScreen("decisionScreen");
-  update_Text();
-});
 
+
+  
+});
+//When back button clicked, play sound, set screen to main screen.
 onEvent("Back_Button", "click", function( ) {
   playSound("sound://category_bell/choose_background.mp3", false);
   setScreen("mainScreen");
